c92d337ad372a586dc76a9c10d58f8cad0440fc1
Clara Raubertas <clara@parallactic.com>
2023-12-13 11:36:10 -0600

    blog formats

--- spec/controllers/rebuilds_controller_spec.rb
+++ spec/controllers/rebuilds_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe RebuildsController, type: :controller do
     @min_site_item_count = 100
     @community_count = 6
     @topic_slug = 'revision-control'
-    @how_to_name = 'How to do Version Control with Git in your CSE Project'
+    @how_to_slug = 'version-control-with-git-in-cse-projects'
     @what_is_slug = 'what-is-revision-control'
     @category_name = 'Better Development'
     @blog_post_slug = 'improve-user-confidence-in-your-software-updates'
@@ -48,7 +48,7 @@ RSpec.describe RebuildsController, type: :controller do
       wi = SearchResult.displayed.find(@what_is_slug)
       expect(wi.topics).to include topic
       expect(topic.site_items).to include wi
-      expect(topic.site_items).to include HowTo.displayed.find_by(name: @how_to_name)
+      expect(topic.site_items).to include SiteItem.displayed.find_by(slug: @how_to_slug)
       expect(topic.category).to eq Category.displayed.find_by(
         name: @category_name
       )
@@ -70,7 +70,7 @@ RSpec.describe RebuildsController, type: :controller do
       # end
       expect(Author.displayed.where(website: 'https://github.com/nniiicc').first.last_name).not_to eq 'Nic'
       expect(Page.find('homepage')).to be_a Page
-      expect(Page.last.snippet).not_to be_empty
+      expect(Page.find('homepage').snippet).not_to be_empty
 
       expect(Author.displayed.where(website: @author_slug).size).to eq 1
       expect(Page.displayed.where(name: 'Contributors')).not_to be_empty
