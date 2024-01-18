5ba7e1e7b51997ee7cada25fa171eeb026dc1d89
Clara Raubertas <clara@parallactic.com>
2024-01-18 10:52:59 -0600

    resource view

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -61,7 +61,8 @@ class ResourcesController < ApplicationController
     @resources = scoped_resources.with_author(@author) if @author
     @resources = @resources.standard_scope
     @resources = SearchResult.displayed.published.order('published_at desc') if params[:recent].to_s == 'true'
-    @total = @resources.size
+    @latest = true
+@total = @resources.size
     @resources = if params[:view] != 'all'
                    @resources.paginate(page: @page_num, per_page: 75)
                  else
