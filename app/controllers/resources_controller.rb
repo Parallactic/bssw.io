2e684c663bc8bc64db472ee66116086843f47c1a
Clara Raubertas <clara@parallactic.com>
2024-03-08 10:53:10 -0600

    page redirect

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -12,6 +12,7 @@ class ResourcesController < ApplicationController
   def show
     @resource = scoped_resources.find_by(slug: params[:id])
     @resource ||= scoped_resources.find(params[:id])
+    redirect_to "/pages/#{@resource.slug}" if @resource.is_a?(Page)
     redirect_to "/events/#{@resource.slug}" if @resource.is_a?(Event)
     redirect_to "/blog_posts/#{@resource.slug}" if @resource.is_a?(BlogPost)
   end
